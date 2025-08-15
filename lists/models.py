ce7a79add649cfbdab72574cb2153ba17bbc1376
Zoe-Perry <zoe.elise7013@gmail.com>
2025-08-15 11:23:15 +0100

    added a user as part of each model

--- lists/models.py
+++ lists/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class List(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="list_author"
+    )
 
     def __str__(self):
         return self.title
@@ -12,6 +15,9 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     list = models.ForeignKey(
         List, on_delete=models.CASCADE, related_name="task"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="task_author"
     )
     created_on = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)

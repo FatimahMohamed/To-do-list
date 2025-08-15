c9c32964e830e202da596f621ec03f4bfdeeeca5
Zoe-Perry <zoe.elise7013@gmail.com>
2025-08-15 11:13:18 +0100

    connected database and added models

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class List(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    list = models.ForeignKey(
+        List, on_delete=models.CASCADE, related_name="task"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
